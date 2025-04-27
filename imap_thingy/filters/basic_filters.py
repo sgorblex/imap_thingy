bad1bcdeac7049d55c306a352da01872a7a339aa
Alessandro Clerici <ale.clericil@gmail.com>
2025-04-27 21:31:28 +0200

    small improvement to basic filters

--- imap_thingy/filters/basic_filters.py
+++ imap_thingy/filters/basic_filters.py
@@ -4,11 +4,11 @@ from imap_thingy.filters.criterion_filter import CriterionFilter, from_is, mark_
 
 class MoveIfFromFilter(CriterionFilter):
     def __init__(self, account: EMailAccount, sender: str, folder: str, mark_read = True):
-        action = move_to(folder) & mark_as_read() if mark_read else move_to(folder)
+        action = mark_as_read() & move_to(folder) if mark_read else move_to(folder)
         super().__init__(account, from_is(sender), action)
 
 
 class MoveIfToFilter(CriterionFilter):
     def __init__(self, account: EMailAccount, sender: str, folder: str, mark_read = True):
-        action = move_to(folder) & mark_as_read() if mark_read else move_to(folder)
+        action = mark_as_read() & move_to(folder) if mark_read else move_to(folder)
         super().__init__(account, to_contains_is(sender), action)
